1c0d3c0017682c6b92755e763e801803a35095be
AlstonS <ashi3454@bths.edu>
2018-10-04 11:45:01 -0400

    classroom

--- src/AngelaJacobWilson/Classroom.java
+++ src/AngelaJacobWilson/Classroom.java
@@ -1,4 +1,16 @@
 package AngelaJacobWilson;
 
 public class Classroom {
+
+    private String firstname;
+    private String lastname;
+    private String subject;
+        public Teacher(String firstname, String lastname, String subject)
+        {
+            super("Teacher");
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.subject = subject;
+
+        }
 }
