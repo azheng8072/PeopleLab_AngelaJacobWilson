a20710f9ca5434dbd88fa7b4cdacf26053f3f479
Angela <azheng8072@bths.edu>
2018-10-05 11:17:35 -0400

    worked on teacher.java

--- src/AngelaJacobWilson/Teacher.java
+++ src/AngelaJacobWilson/Teacher.java
@@ -3,4 +3,19 @@ package AngelaJacobWilson;
 public class Teacher extends Person{
     private String subject;
     private String title;
+
+    public Teacher(String subject, String title, String firstName, String familyName)
+    {
+        super(firstName, familyName);
+        this.subject = subject;
+        this.title = title;
+    }
+    public getSubject()
+    {
+        return this.subject;
+    }
+    public toString()
+    {
+        return this.title + ". " + this.getFamilyName();
+    }
 }
