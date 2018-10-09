8f6d619d77e6091023fe5eddd040aa1c28c1be80
azheng8072 <43213098+azheng8072@users.noreply.github.com>
2018-10-09 11:10:07 -0400

    commented a bit

--- src/AngelaJacobWilson/Teacher.java
+++ src/AngelaJacobWilson/Teacher.java
@@ -1,19 +1,22 @@
 package AngelaJacobWilson;
 
 public class Teacher extends Person{
+// created strings for subject and title
     private String subject;
     private String title;
-
+// created constructor and super
     public Teacher(String subject, String title, String firstName, String familyName)
     {
         super(firstName, familyName);
         this.subject = subject;
         this.title = title;
     }
+//returns subject based on what was given
     public String getSubject()
     {
         return this.subject;
     }
+//returns the name of the person in formal form ie: input CS, Mr, Nathan Folwell -> CS Mr. Folwell
     public String toString()
     {
         return this.title + ". " + this.getFamilyName();
